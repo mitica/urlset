d275de51b3e249b06e90d3eebea8775df238f7dd
Dumitru Cantea <canteadumitru@gmail.com>
2015-02-20 18:43:35 +0200

    fix hierarchy

--- test/t1.js
+++ test/t1.js
@@ -1,14 +1,19 @@
 var urlset = require('../lib/urlset.js'),
-path=require('path');
+  path = require('path');
 
-urlset.load(__dirname+'/t1.json');
+urlset.init({debug: false}).load(__dirname + '/t1.json');
 
-console.log(urlset._data);
+//console.log(urlset._data);
 console.log(urlset.url);
 
 console.log(urlset.url.home());
 console.log(urlset.url.news());
 console.log(urlset.url.news.item(12));
-console.log(urlset.url.news({ul:'ro', param:'#$%% ,./<>>?'}));
+console.log(urlset.url.news({
+  ul: 'ro',
+  param: '#$% ,./<>>?'
+}));
 console.log(urlset.url.news.id(121332));
+console.log(urlset.url.news.id.old());
+console.log(urlset.url.news.controls.places());
 //console.log(urlset.url.news.item());
