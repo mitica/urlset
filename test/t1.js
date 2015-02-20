158b7343691ec670957bc70250cc6893194569ad
Dumitru Cantea <canteadumitru@gmail.com>
2015-02-20 19:36:11 +0200

    fix countArgs

--- test/t1.js
+++ test/t1.js
@@ -1,7 +1,7 @@
 var urlset = require('../lib/urlset.js'),
   path = require('path');
 
-urlset.init({debug: false}).load(__dirname + '/t1.json');
+urlset.init({debug: true}).load(__dirname + '/t1.json');
 
 //console.log(urlset._data);
 console.log(urlset.url);
@@ -9,6 +9,7 @@ console.log(urlset.url);
 console.log(urlset.url.home());
 console.log(urlset.url.news());
 console.log(urlset.url.news.item(12));
+console.log(urlset.url.news.item2(12, 23));
 console.log(urlset.url.news({
   ul: 'ro',
   param: '#$% ,./<>>?'
